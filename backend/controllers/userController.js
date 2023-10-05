2d1ae034c0db6ee4fee2a6e725ca58570aba40b7
Leo Harada <leotharada@gmail.com>
2023-10-05 00:07:46 -0700

    Completed restAPI routes and CRUD operations for the backend. Users can now follow/unfollow, like/unlike, create a post, reply to a post, delete a post, and ged their feed from the users they're following. Will not work on connecting the frontend with the backend.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,22 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import tokenAndCookie from "../utils/helpers/tokenAndCookie.js";
 
+const getUserProfile = async (req, res) => {
+    const { username } = req.params;
+
+    try {
+        const user = await User.findOne({ username })
+            .select("-password")
+            .select("-updatedAt");
+        if (!user) return res.status(400).json({ error: "User not found." });
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in getUserProfile: ", err.message);
+    }
+};
+
 const signupUser = async (req, res) => {
     try {
         const { name, email, username, password } = req.body;
@@ -64,7 +80,7 @@ const loginUser = async (req, res) => {
         });
     } catch (err) {
         res.status(500).json({ error: err.message });
-        conesole.log("Error in loginUser: ", err.message);
+        console.log("Error in loginUser: ", err.message);
     }
 };
 
@@ -77,7 +93,7 @@ const logoutUser = (req, res) => {
         });
     } catch (err) {
         res.status(500).json({ error: err.message });
-        conesole.log("Error in logoutUser: ", err.message);
+        console.log("Error in logoutUser: ", err.message);
     }
 };
 
@@ -116,8 +132,52 @@ const followUnfollow = async (req, res) => {
         }
     } catch (err) {
         res.status(500).json({ error: err.message });
-        conesole.log("Error in followUnfollow: ", err.message);
+        console.log("Error in followUnfollow: ", err.message);
     }
 };
 
-export { signupUser, loginUser, logoutUser, followUnfollow };
+const updateUser = async (req, res) => {
+    const { name, email, username, password, profilePic, bio } = req.body;
+    const userId = req.user._id;
+
+    try {
+        let user = await User.findById(userId);
+        if (!user) return res.status(400).json({ error: "User not found." });
+
+        if (req.params.id !== userId.toString())
+            return res
+                .status(400)
+                .json({ error: "You cannot update other user's profile" });
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+
+        user = await user.save();
+
+        res.status(200).json({
+            message: "Profile updated successfully.",
+            user,
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in updateUser: ", err.message);
+    }
+};
+
+export {
+    signupUser,
+    loginUser,
+    logoutUser,
+    followUnfollow,
+    updateUser,
+    getUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,21 @@
 import express from "express";
 import {
     followUnfollow,
+    getUserProfile,
     loginUser,
     logoutUser,
     signupUser,
+    updateUser,
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnfollow);
+router.post("/update/:id", protectRoute, updateUser);
 
 export default router;
